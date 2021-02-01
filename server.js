441f8f7cfae92d39b512e2788cf028aa1206c507
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-01 10:15:42 -0500

    intial

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const UrlSchema = new mongoose.Schema({
+    url : String,
+    hash :  Number,
+  });
+
+const Url = mongoose.model('Url', UrlSchema);
+
+
+const createAndSaveUrl = function(done) {
+  var newUrl = new Url({url: "http://www.google.com", hash: 42});
+  newUrl.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
+
+
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
