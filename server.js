6b31695c9bad637e31a2a169998bfacad5967d80
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-16 11:46:48 -0500

    connected to db

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const dns = require('dns');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+
+var Url;
+var shortUrl;
+var originalUrl;
 
 const db = mongoose.connection;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,34 +17,49 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   const UrlSchema = new mongoose.Schema({
-    url : String,
+    original_url : String,
+    short_url : String
   });
     Url = mongoose.model('Url', UrlSchema);
 });
 
-var Url;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 
-
-
-
-
-
 app.post('/api/shorturl/new', (req,res) => {
-  console.log(req.body)
-  const newUrl = new Url(req.body);
+  let regex = /(\.)(\w)/;
+  originalUrl = req.body.url;
+  shortUrl = req.body.url.match(/(\.)(\w)/)[2];
+  console.log('success', originalUrl, shortUrl)
+
+  const newUrl = new Url({
+    original_url: originalUrl,
+    short_url: shortUrl,
+  })
+
   newUrl.save()
+
+
     .then(item =>{
-      res.send("Information saved to database");
+    res.send({
+      original_url :originalUrl,
+      short_url : shortUrl
+    })
     })
     .catch(err =>{
       res.status(400).send("Unable to save to database");
     });
 });
+
+// Your first API endpoint
+app.get('/api/shorturl/:suffix', (req, res) => {
+
+});
+
+
 
 
 //   const lookup = dns.lookup(req.body)
@@ -61,11 +81,6 @@ const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-
-// Your first API endpoint
-app.post('/api/shorturl/new', function(req, res) {
 });
 
 
